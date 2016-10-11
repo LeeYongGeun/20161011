7c7f811404ee55f10ae3539dafd6290a920f3216
LeeYongGeun <lyg5692@naver.com>
2016-10-11 18:28:21 +0900

    Initial commit

---
+++ 20161011.py
@@ -0,0 +1,16 @@
+number = 23
+running = True
+
+while running:
+    guess =int(input('Enter an integer : '))
+    if guess == number:
+        print('Congratylations, You guessed it. ')
+        # running = False       ctrl+/
+        break
+    elif guess < number :
+        print('No, It is a little higher than that. ')
+    else:
+        print('No, It is a little lower than that')
+else:
+    print('The while loop is over') # break구문 사용시 else는 작동안됨
+print('Done')
